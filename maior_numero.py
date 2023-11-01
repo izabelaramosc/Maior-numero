cc6ef9f566da0ca113bb235bbca0e50dd703ef84
Izabela Ramos <146668672+izabelaramosc@users.noreply.github.com>
2023-10-31 21:09:30 -0300

    primeiro commit

---
+++ maior_numero.py
@@ -0,0 +1,8 @@
+primeiro_valor = input ('digite um valor ') 
+segundo_valor = input ('digite um valor ')
+
+if primeiro_valor >= segundo_valor:
+  print (f'{primeiro_valor=} é maior ou igual ao {segundo_valor=}')
+else:
+   print (f'{segundo_valor=} é maior do que {primeiro_valor=}')
+
